0a451191104d5f852d073f3ffc4e31d94e1350ec
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-04-25 13:31:29 +0500

    theme compose

---
+++ app/src/main/java/xyz/teamgravity/todoapi/compose/component/ToDoCard.kt
@@ -0,0 +1,16 @@
+package xyz.teamgravity.todoapi.compose.component
+
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ToDoCard(
+    task: String,
+    isChecked: Boolean
+) {
+    Card(
+
+    ) {
+
+    }
+}
---
+++ app/src/main/java/xyz/teamgravity/todoapi/compose/theme/Theme.kt
@@ -0,0 +1,34 @@
+package xyz.teamgravity.todoapi.compose.theme
+
+import android.annotation.SuppressLint
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@SuppressLint("ConflictingOnColor")
+private val LightThemeColors = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Black2,
+    secondary = Color.White,
+    secondaryVariant = Teal300,
+    onSecondary = Color.Black,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Black2,
+)
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colors = LightThemeColors,
+        typography = QuickSandTypography,
+        shapes = AppShapes
+    ) {
+        content()
+    }
+}
