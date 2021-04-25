1b400a30b3d0fde9933e4a2617f81e60e2d186bb
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-04-25 14:28:27 +0500

    final shit

--- app/src/main/java/xyz/teamgravity/todoapi/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/todoapi/activity/MainActivity.kt
@@ -2,27 +2,45 @@ package xyz.teamgravity.todoapi.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.todoapi.arch.viewmodel.MainViewModel
 import xyz.teamgravity.todoapi.compose.component.IndefiniteProgressBar
+import xyz.teamgravity.todoapi.compose.component.ToDoCard
 import xyz.teamgravity.todoapi.compose.theme.AppTheme
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewmodel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+
+            val loading = viewmodel.loading.value
+
+            val todos = viewmodel.todos.value
+
             AppTheme {
                 Box(modifier = Modifier.fillMaxSize()) {
                     LazyColumn {
-
+                        items(todos) { todo ->
+                            ToDoCard(
+                                task = todo.title,
+                                isChecked = todo.completed
+                            )
+                        }
                     }
 
-                    IndefiniteProgressBar(isDisplayed = true)
+                    IndefiniteProgressBar(isDisplayed = loading)
                 }
             }
         }
