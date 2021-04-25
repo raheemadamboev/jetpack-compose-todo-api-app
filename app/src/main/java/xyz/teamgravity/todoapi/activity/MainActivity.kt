32fe12ff5cca0ebe7935a5316cd67a09638fd2b5
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-04-25 13:13:36 +0500

    icon

--- app/src/main/java/xyz/teamgravity/todoapi/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/todoapi/activity/MainActivity.kt
@@ -1,13 +1,13 @@
-package xyz.teamgravity.todoapi
+package xyz.teamgravity.todoapi.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.teamgravity.todoapi.R
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
